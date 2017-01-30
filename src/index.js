76501a752b8a77e11e09e2106f61ff682c9a3aae
Gar <gar@lonelydatu.com>
2017-01-30 16:55:09 -0500

    feat(build issue resolved): included babel-loader

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import uniqueRandomArray from 'unique-random-array';
+
+
+module.exports = {
+  all: 'all',
+  random: random,
+  gar(){
+    return 55
+  }
+};
+
+function random(number) {
+ return 'rand'
+}
+
