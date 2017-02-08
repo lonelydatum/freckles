af2a3d8dc3e2b44afc2d0366bcebf4db8d3edd4f
Gar <gar@lonelydatu.com>
2017-02-07 21:56:20 -0500

    feat(fixed entry file): entry

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,7 +3,7 @@ import {join} from 'path'
 const include = join(__dirname, 'src')
 
 export default {
-  entry: './src/index',
+  entry: './src/Stardust.js',
   output: {
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
